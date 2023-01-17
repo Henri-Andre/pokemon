d3330af62714f782f2349beb67044da568afef91
guillou <henri-andre.guillou@3wa.io>
2023-01-17 12:03:41 +0100

    first commit

---
+++ assets/js/fetchpokemons.js
@@ -0,0 +1,24 @@
+const colors = {
+	fire: '#FDDFDF',
+	grass: '#DEFDE0',
+	electric: '#DBC461',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5',
+    ice:'#005b96',
+    dark: '#333333',
+    steel:'#99aab5',
+    sprectrum:'#800080'
+};
+
+
+
+export{colors};
---
+++ assets/js/script.js
@@ -0,0 +1,101 @@
+async function getPokemon(){
+    // co qui attend une promise
+    let promise = await fetch("https://pokebuildapi.fr/api/v1/pokemon");
+    // verif si co est ok
+    if(promise.ok == true){
+        // trnsformation en tableau json
+        let pokemons = await promise.json();
+        //je return mon tableau json pokemons
+        return pokemons;
+    }else{
+        // si error
+    }
+}
+
+import { colors } from "./fetchpokemons.js";
+
+
+
+
+
+
+async function showPokemons(pokemons){
+    const pokedexElement = document.getElementById('pokedexContainer');
+    pokemons.forEach(pokemon => {
+        const articleElement = document.createElement('article');
+        const spanElement = document.createElement('span')
+        const h2Element = document.createElement('h2');
+        const img = document.createElement('img');
+        const btnElement = document.createElement('button');
+
+        img.setAttribute('src', pokemon.image );
+        img.setAttribute('alt', pokemon.name );
+        articleElement.setAttribute('id','pokemonByType');
+        
+
+
+        const namePokemon = document.createTextNode(pokemon.name);
+        const typePokemon = document.createTextNode('Type : ' + pokemon.apiTypes[0].name);
+        const btn =  document.createTextNode('Statisque');
+
+        const colorElement = pokemon.apiTypes[0].name
+
+        if (colorElement  === "Feu" ){
+
+            articleElement.style.background = colors.fire;
+            } else if ( colorElement === "Poison" ){
+                articleElement.style.background = colors.poison;
+            } else if ( colorElement === "Électrik" ){
+                articleElement.style.background = colors.electric;
+            } else if ( colorElement === "Eau" ){
+                articleElement.style.background = colors.water;
+            }else if ( colorElement === "Vol" ){
+                articleElement.style.background = colors.flying;
+            }else if ( colorElement === "Normal" ){
+                articleElement.style.background = colors.normal;
+            }else if ( colorElement === "Combat" ){
+                articleElement.style.background = colors.fighting;
+            }else if ( colorElement === "Fée" ){
+                articleElement.style.background = colors.fairy;
+            }else if ( colorElement === "Insecte" ){
+                articleElement.style.background = colors.bug;
+            }else if ( colorElement === "Sol" ){
+                articleElement.style.background = colors.ground;
+            }else if ( colorElement === "Roche" ){
+                articleElement.style.background = colors.rock;
+            }else if ( colorElement === "Psy" ){
+                articleElement.style.background = colors.psychic;
+            }else if ( colorElement === "Plante" ){
+                articleElement.style.background = colors.grass;
+            }else if ( colorElement === "Dragon" ){
+                articleElement.style.background = colors.dragon;
+            }else if ( colorElement === "Spectre" ){
+                articleElement.style.background = colors.steel;
+            }else if ( colorElement === "Glace" ){
+                articleElement.style.background = colors.ice;
+            }else if ( colorElement === "Acier" ){
+                articleElement.style.background = colors.steel;
+            }else if ( colorElement === "Ténèbres" ){
+                articleElement.style.background = colors.dark;
+                articleElement.style.color = "#FFF"
+            }        
+      
+        console.log(typePokemon);
+
+        
+        h2Element.appendChild(namePokemon);
+        spanElement.appendChild(typePokemon);
+        btnElement.appendChild(btn);
+
+        pokedexElement.appendChild(articleElement);
+        articleElement.append(h2Element,img,spanElement,btnElement);
+
+       
+
+       
+        
+    });
+    
+}
+
+getPokemon().then(pokemons => showPokemons(pokemons));
